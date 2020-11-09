c5f1b26f20864ed8e6279f9f6521eb60d896dd86
Mikhail Merzlyutin <merzlyutinme@gmail.com>
2020-11-09 18:39:21 +0300

    Collect World info

--- src/hdusd/export/world.py
+++ src/hdusd/export/world.py
@@ -22,10 +22,9 @@ from pathlib import Path
 import bpy
 import mathutils
 
-from pxr import UsdLux
+from pxr import Sdf, UsdLux
 
 from . import image, sdf_path
-from .image import ImagePixels
 
 from ..utils import logging
 log = logging.Log(tag='export.world')
@@ -35,79 +34,103 @@ WARNING_IMAGE_NOT_DEFINED_COLOR = (1.0, 0.0, 1.0)
 STUDIO_LIGHT_DEFAULT_COLOR = (0.051, 0.051, 0.051)  # Blender's default background color in viewport
 
 
-def set_light_rotation(rpr_light, rotation: Tuple[float]) -> np.array:
-    """ Calculates rotation matrix from gizmo rotation """
+@dataclass(init=False, eq=True, repr=True)
+class WorldData:
+    """ Comparable dataclass which holds all environment settings """
 
-    matrix = np.identity(4, dtype=np.float32)
-    euler = mathutils.Euler((rotation[0], rotation[1], rotation[2] - np.pi / 2))
+    @dataclass(eq=True)
+    class IblData:
+        image: str = None
+        studio_light: str = None
+        rotation: tuple = (0, 0, 0)
 
-    rotation_matrix = np.array(euler.to_matrix(), dtype=np.float32)
-    fixup = np.array([[1, 0, 0],
-                      [0, 0, 1],
-                      [0, 1, 0]], dtype=np.float32)
+        @staticmethod
+        def init_from_cycles(world: bpy.types.World):
+            data = WorldData.IblData()
 
-    matrix[:3, :3] = np.dot(fixup, rotation_matrix)
-    rpr_light.set_transform(matrix, False)
-    return matrix
+            data.color = world.color
+
+            if not world.use_nodes:
+                return data
+
+            # Parse simplest environment IBL image nodes config:
+            # Mapping -> Environment Image -> Background -> World Output
+            output = world.node_tree.get_output_node('ALL')
+            if not output:
+                output = world.node_tree.get_output_node('CYCLES')
+
+            if not output or not output.inputs['Surface'].is_linked:
+                return data
+
+            # Background node
+            background_node = output.inputs['Surface'].links[0].from_node
+            if background_node.type != 'BACKGROUND':
+                return data
+
+            data.color = background_node.inputs['Color'].default_value
+
+            # Environment Image node => image file info
+            if not background_node.inputs['Color'].is_linked:
+                return data
+            environment_node = background_node.inputs['Color'].links[0].from_node
+            if environment_node.type != 'TEX_ENVIRONMENT':
+                return data
+            if not environment_node.image or not environment_node.image.filepath:
+                return data
+            img_path = environment_node.image.filepath_from_user()
+            if not Path(img_path).is_file():
+                return data
+
+            # TODO extract studio lights and packed images
+            data.image = str(img_path)
+
+            # Mapping node => rotation info
+            mapping_node = environment_node.inputs['Vector'].links[0].from_node
+            if mapping_node.type != 'BACKGROUND':
+                return data
+            data.rotation = tuple(mapping_node.inputs[1])
+
+            return data
+
+        @staticmethod
+        def init_from_shading(shading):
+            data = WorldData.IblData()
+            data.studio_light = shading.studio_light
+            return data
+
+    cycles_ibl: IblData = None
+    # TODO support studio light settings
+    # TODO support RPR Environment settings as an option
+
+    @staticmethod
+    def init_from_world(world: bpy.types.World):
+        """ Returns WorldData from bpy.types.World """
+        data = WorldData()
+
+        data.cycles_ibl = WorldData.IblData.init_from_cycles(world)
+
+        return data
 
 
 def sync(obj_prim, world: bpy.types.World, **kwargs):
     is_gl_mode = kwargs.get('is_gl_delegate', False)
 
-    # TODO add RPR Environment settings support as an option
-    if not world.use_nodes:
+    # TODO export correct Dome light with texture for GL mode
+    if is_gl_mode:
         return
 
-    color = world.color
-    ibl_path = ""
-    if world.use_nodes:
-        # Parse simplest environment IBL image nodes config
-        output = world.node_tree.get_output_node('ALL')
-        if not output:
-            output = world.node_tree.get_output_node('CYCLES')
-
-        if not output or not output.inputs['Surface'].is_linked:
-            return
-
-        linked_node = output.inputs['Surface'].links[0].from_node
-        if linked_node.type != 'BACKGROUND':
-            return
-
-        color = linked_node.inputs['Color'].default_value
-        if linked_node.inputs['Color'].is_linked:
-            color_node = linked_node.inputs['Color'].links[0].from_node
-            if color_node.type != 'TEX_ENVIRONMENT':
-                return
-            if not color_node.image or not color_node.image.filepath:
-                return
-            img_path = color_node.image.filepath_from_user()
-            if not Path(img_path).is_file():
-                return
-
-            ibl_path = str(img_path)
+    data = WorldData.init_from_world(world)
+    log.info(f"world data: {data}")
 
     stage = obj_prim.GetStage()
 
-
-    if is_gl_mode:
-        try:
-            usd_light = UsdLux.DomeLight.Define(
-                stage, f"{obj_prim.GetPath()}/{sdf_path(world.name)}")
-
-            if ibl_path:
-                from pxr import Sdf
-                p = Sdf.AssetPath(ibl_path)
-                usd_light.CreateTextureFileAttr(p)
-        except Exception as e:
-            log.warn(f"World export error: {type(e)};\n\t{str(e)}\n")
-    else:
+    if data.cycles_ibl.image:
         usd_light = UsdLux.DomeLight.Define(
             stage, f"{obj_prim.GetPath()}/{sdf_path(world.name)}")
 
-        if ibl_path:
-            from pxr import Sdf
-            p = Sdf.AssetPath(ibl_path)
-            usd_light.CreateTextureFileAttr(p)
+        p = Sdf.AssetPath(data.cycles_ibl.image)
+        usd_light.CreateTextureFileAttr(p)
+        usd_light.OrientToStageUpAxis()
 
-    # data = WorldData.init_from_world(world)
-    # data.export(rpr_context)
+        # TODO apply rotation
+    # TODO use color data if no image present
