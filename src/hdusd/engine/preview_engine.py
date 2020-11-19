a574cf50a4735b1c366bd24c4c6a016271638695
Mikhail Merzlyutin <merzlyutinme@gmail.com>
2020-11-19 17:56:11 +0300

    Basic Preview sync

--- src/hdusd/engine/preview_engine.py
+++ src/hdusd/engine/preview_engine.py
@@ -16,21 +16,53 @@ import bpy
 
 from .engine import Engine
 from .final_engine import FinalEngine
+from ..export import depsgraph as dg
+
+import numpy as np
+
+from pxr import UsdImagingGL, UsdImagingLite
 
 from ..utils import logging
 log = logging.Log(tag='PreviewEngine')
 
 
 class PreviewEngine(FinalEngine):
+    """ Render engine for preview material, lights, environment """
+
     TYPE = 'PREVIEW'
 
     def render(self, scene):
         return
 
     def sync(self, depsgraph):
+
+        def notify_callback(info):
+            log(0.0, info)
+
+        def test_break():
+            return self.render_engine.test_break()
+
         scene = depsgraph.scene
+        view_layer = depsgraph.view_layer
         settings_scene = bpy.context.scene
+        self.is_gl_delegate = scene.hdusd.final.is_opengl
+        self.render_layer_name = view_layer.name
 
-        resolution = (scene.render.resolution_x, scene.render.resolution_y)
+        self.width = scene.render.resolution_x
+        self.height = scene.render.resolution_y
+        screen_ratio = self.width / self.height
+        log(f"screen {self.width}x{self.height}, ratio {screen_ratio}")
 
-        return
+        if not self.stage:
+            self.stage = dg.sync(
+                depsgraph,
+                screen_ratio=screen_ratio,
+                is_gl_delegate=self.is_gl_delegate,
+                notify_callback=notify_callback,
+                test_break=test_break,
+                engine=self,
+            )
+
+        self.render_engine.bl_use_gpu_context = self.is_gl_delegate
+
+        log(f"Sync finished")
