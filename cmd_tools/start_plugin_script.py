bd556a55dadfebbb76fb601695fd1365f85624ed
Bogdan Nagirniak <bnagirniak@luxoft.com>
2021-01-28 16:06:05 +0200

    Improved cmd_tools scripts

--- cmd_tools/start_plugin_script.py
+++ cmd_tools/start_plugin_script.py
@@ -16,20 +16,16 @@
 #script to load addon from within blender text editor
 
 import faulthandler
+from pathlib import Path
+import sys
+
+import bpy
+
+sys.path.append(str((Path(__file__).parent.parent / 'src').resolve()))
 
 faulthandler.enable()
-
-from pathlib import Path
-
-src_path = str((Path(__file__).parent.parent/'src').resolve())
-
-import sys
-if src_path not in sys.path:
-    sys.path.append(src_path)
 
 import hdusd
 
 hdusd.register()
-
-import bpy
 bpy.context.scene.render.engine = 'HdUSD'
