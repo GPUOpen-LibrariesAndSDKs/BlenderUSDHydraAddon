977c5cb16f4c03058ef2c7e05e792f09c351c9cb
Bogdan Nagirniak <bnagirniak@luxoft.com>
2021-02-24 22:55:43 +0200

    New import algorithm

--- src/hdusd/mx_nodes/node_tree.py
+++ src/hdusd/mx_nodes/node_tree.py
@@ -62,7 +62,9 @@ class MxNodeTree(bpy.types.ShaderNodeTree):
 
         return doc
 
-    def import_(self, doc: mx.Document):
+    def import_old(self, doc: mx.Document):
+        self.nodes.clear()
+
         loc_x, loc_y = 0, 0
 
         mx_nodes = [n for n in doc.getNodes() if n.getCategory() == 'surfacematerial']
@@ -76,6 +78,7 @@ class MxNodeTree(bpy.types.ShaderNodeTree):
             for mx_node in mx_nodes:
                 MxNode_cls = get_mx_node_cls(mx_node.getCategory(), mx_node.getType())
                 node = self.nodes.new(MxNode_cls.bl_idname)
+                node.name = mx_node.getName()
 
                 node.data_type = mx_node.getType()
                 for mx_param in mx_node.getParameters():
@@ -95,8 +98,8 @@ class MxNodeTree(bpy.types.ShaderNodeTree):
                         new_links.append((node_name, node.name, mx_input.getName()))
 
                 # setting links connected from created node
-                for mx_name, node_name, in_key in links:
-                    if mx_name == mx_node.getName():
+                for out_node_name, node_name, in_key in links:
+                    if out_node_name == node.name:
                         self.links.new(node.outputs[0], self.nodes[node_name].inputs[in_key])
 
                 node.location = (loc_x, loc_y)
@@ -106,6 +109,42 @@ class MxNodeTree(bpy.types.ShaderNodeTree):
             loc_x -= layer_width + NODE_SEPARATION_WIDTH
             mx_nodes = new_mx_nodes
             links = new_links
+
+    def import_(self, doc: mx.Document):
+        self.nodes.clear()
+
+        def import_node(mx_node):
+            name = mx_node.getName()
+            if name in self:
+                return self[name]
+
+            MxNode_cls = get_mx_node_cls(mx_node.getCategory(), mx_node.getType())
+            node = self.nodes.new(MxNode_cls.bl_idname)
+            node.name = name
+
+            node.data_type = mx_node.getType()
+            for mx_param in mx_node.getParameters():
+                node.set_param_value(mx_param.getName(),
+                                     mx_utils.parse_value(mx_param.getValue(), mx_param.getType()))
+
+            for mx_input in mx_node.getInputs():
+                input_name = mx_input.getName()
+                val = mx_input.getValue()
+                if val is not None:
+                    node.set_input_default(input_name,
+                                           mx_utils.parse_value(val, mx_input.getType()))
+                    continue
+
+                node_name = mx_input.getNodeName()
+                if node_name:
+                    new_mx_node = doc.getNode(node_name)
+                    new_node = import_node(new_mx_node)
+                    self.links.new(new_node.outputs[0], node.inputs[input_name])
+
+            return node
+
+        mx_node = next(n for n in doc.getNodes() if n.getCategory() == 'surfacematerial')
+        import_node(mx_node)
 
     def create_basic_nodes(self, node_name='PBR_standard_surface'):
         """ Reset basic node tree structure using scene or USD file as an input """
