0662906018553fd0ebf4a547baf1f14ff07d06d5
Mikhail Merzlyutin <merzlyutinme@gmail.com>
2021-03-31 16:53:48 +0300

    Hidden Cycles and Eevee shader menus

--- src/hdusd/bl_nodes/__init__.py
+++ src/hdusd/bl_nodes/__init__.py
@@ -12,5 +12,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #********************************************************************
+from nodeitems_builtins import (
+    ShaderNodeCategory,
+)
+
 from ..utils import logging
 log = logging.Log("bl_nodes")
+
+
+# some nodes are hidden from plugins by Cycles itself(like Material Output), some we could not support.
+# thus we'll hide 'em all to show only selected set of supported Blender nodes
+# custom HdUSD_CompatibleShaderNodeCategory will be used instead
+def hide_cycles_and_eevee_poll(method):
+    @classmethod
+    def func(cls, context):
+        return not context.scene.render.engine == 'HdUSD' and method(context)
+    return func
+
+
+old_shader_node_category_poll = None
+
+
+def register():
+    # hide Cycles
+    global old_shader_node_category_poll
+    old_shader_node_category_poll = ShaderNodeCategory.poll
+    ShaderNodeCategory.poll = hide_cycles_and_eevee_poll(ShaderNodeCategory.poll)
+
+
+def unregister():
+    if old_shader_node_category_poll and ShaderNodeCategory.poll is not old_shader_node_category_poll:
+        ShaderNodeCategory.poll = old_shader_node_category_poll
