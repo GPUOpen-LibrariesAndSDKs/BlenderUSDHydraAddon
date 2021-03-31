1eaac95fa65f393af15b3224e1b879ef45d24fbc
Mikhail Merzlyutin <merzlyutinme@gmail.com>
2021-03-31 21:45:56 +0300

    Added support for Converter - Math node

--- src/hdusd/bl_nodes/nodes/shader.py
+++ src/hdusd/bl_nodes/nodes/shader.py
@@ -197,50 +197,66 @@ class ShaderNodeMath(NodeParser):
     """ Map Blender operations to MaterialX definitions, see the stdlib_defs.mtlx in MaterialX """
 
     def export(self):
-        result = None
-        node_name = ''
-        node_type = ''
-        node_data = {}
-
         op = self.node.operation
         in1 = self.get_input_value(0)
-        in2 = None
-        in3 = None
-        in1_type = ''
-        operands_type = ''
-        in2_type = ''
-        in3_type = ''
-        if in1:
-            log.info(f"in1.data: {in1.data}; {type(in1)}")
-            if isinstance(in1.data, float):
-                operands_type = 'float'
-            elif isinstance(in1.data, tuple):
-                if len(in1.data) == 2:
-                    operands_type = 'vector2'
-                elif len(in1.data) == 3:
-                    operands_type = 'vector3'
-                elif len(in1.data) == 4:
-                    operands_type = 'vector4'
-            in2 = self.get_input_value(0)
-        log.info(f"operatands_type = {operands_type}")
+        # single operand operations
+        if op == 'SINE':
+            res = in1.sin()
+        elif op == 'COSINE':
+            res = in1.cos()
+        elif op == 'TANGENT':
+            res = in1.tan()
+        elif op == 'ARCSINE':
+            res = in1.asin()
+        elif op == 'ARCCOSINE':
+            res = in1.acos()
+        elif op == 'ARCTANGENT':
+            res = in1.atan()
+        elif op == 'LOGARITHM':
+            res = in1.log()
+        elif op == 'ABSOLUTE':
+            res = abs(in1)
+        elif op == 'FLOOR':
+            res = in1.floor()
+        elif op == 'FRACT':
+            res = in1 % 1.0
+        elif op == 'CEIL':
+            res = in1.ceil()
+        elif op == 'ROUND':
+            f = in1.floor()
+            res = (in1 % 1.0 < 0.5).if_else(f, f + 1.0)
 
-        if op == 'ADD':
-            node_name = 'add'
-            node_type = 'surfaceshader'
-            node_data = {
-                'in1': in1,
-                'in2': in2,
-            }
+        else:  # 2-operand operations
+            in2 = self.get_input_value(1)
 
-        if node_name and node_type:
-            node_name = f"{node_name}_{operands_type}"
-            node_type = operands_type
-            result = self.create_node(node_name, node_type, node_data)
+            if op == 'ADD':
+                res = in1 + in2
+            elif op == 'SUBTRACT':
+                res = in1 - in2
+            elif op == 'MULTIPLY':
+                res = in1 * in2
+            elif op == 'DIVIDE':
+                res = in1 / in2
+            elif op == 'POWER':
+                res = in1 ** in2
+            elif op == 'MINIMUM':
+                res = in1.min(in2)
+            elif op == 'MAXIMUM':
+                res = in1.max(in2)
 
-        log.info(f"node_name {node_name}; node_type {node_type}")
-        log.info(f"result {result}")
+            else:
+                in3 = self.get_input_value(2)
 
-        return result
+                if op == 'MULTIPLY_ADD':
+                    res = in1 * in2 + in3
+                else:
+                    log.warn("Unsupported math operation", op)
+                    return None
+
+        if self.node.use_clamp:
+            res = res.clamp()
+
+        return res
 
 
 class ShaderNodeMixShader(NodeParser):
