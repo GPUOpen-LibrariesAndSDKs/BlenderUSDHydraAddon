9e60dccbd77b08d2fe1a023f84cef2d9e0fca6e8
Bogdan Nagirniak <bnagirniak@luxoft.com>
2021-04-27 02:03:23 +0300

    Added jobs parameter to tools/build.py

--- tools/build.py
+++ tools/build.py
@@ -24,63 +24,55 @@ OS = platform.system()
 repo_dir = Path(__file__).parent.parent.absolute()
 
 
-def usd():
+def usd(jobs):
     import build_usd
-    build_usd.main("--no-usdview")
+    args = ["--no-usdview"]
+    if jobs > 0:
+        args += ['-j', str(jobs)]
+
+    build_usd.main(*args)
 
 
-def hdrpr(compiler):
+def _cmake(ch_dir, compiler, jobs, args):
     cur_dir = os.getcwd()
-    os.chdir(str(repo_dir / "deps/HdRPR"))
+    os.chdir(str(ch_dir))
 
-    args = [
-        'cmake', '-B', 'build',
-        f'-Dpxr_DIR={repo_dir / "bin/USD/install"}',
-        f'-DCMAKE_INSTALL_PREFIX={repo_dir / "bin/HdRPR/install"}',
-        '-DRPR_BUILD_AS_HOUDINI_PLUGIN=FALSE'
-    ]
+    build_args = ['-B', 'build', *args]
     if compiler:
-        args += ['-G', compiler]
+        build_args += ['-G', compiler]
+
+    compile_args = [
+        '--build', 'build',
+        '--config', 'Release',
+        '--target', 'install'
+    ]
+    if jobs > 0:
+        compile_args += ['--', '-j', str(jobs)]
 
     try:
-        subprocess.check_call(args)
-
-        subprocess.check_call([
-            'cmake', '--build', 'build',
-            '--config', 'Release',
-            '--target', 'install'
-        ])
+        subprocess.check_call(['cmake', *build_args])
+        subprocess.check_call(['cmake', *compile_args])
 
     finally:
         os.chdir(cur_dir)
 
 
-def materialx(compiler):
-    cur_dir = os.getcwd()
-    os.chdir(str(repo_dir / "deps/HdRPR/deps/MaterialX"))
+def hdrpr(compiler, jobs):
+    _cmake(repo_dir / "deps/HdRPR", compiler, jobs, [
+        f'-Dpxr_DIR={repo_dir / "bin/USD/install"}',
+        f'-DCMAKE_INSTALL_PREFIX={repo_dir / "bin/HdRPR/install"}',
+        '-DRPR_BUILD_AS_HOUDINI_PLUGIN=FALSE'
+    ])
 
-    args = [
-        'cmake', '-B', 'build',
+
+def materialx(compiler, jobs):
+    _cmake(repo_dir / "deps/HdRPR", compiler, jobs, [
         f'-DCMAKE_INSTALL_PREFIX={repo_dir / "bin/MaterialX/install"}',
         '-DMATERIALX_BUILD_PYTHON=ON',
         f'-DMATERIALX_PYTHON_EXECUTABLE={sys.executable}',
         '-DMATERIALX_INSTALL_PYTHON=OFF',
         '-DMATERIALX_BUILD_VIEWER=ON'
-    ]
-    if compiler:
-        args += ['-G', compiler]
-
-    try:
-        subprocess.check_call(args)
-
-        subprocess.check_call([
-            'cmake', '--build', 'build',
-            '--config', 'Release',
-            '--target', 'install'
-        ])
-
-    finally:
-        os.chdir(cur_dir)
+    ])
 
 
 def libs():
@@ -119,6 +111,8 @@ def main():
     ap.add_argument("-G", required=False, type=str, default="",
                     help="Compiler for HdRPR and MaterialX in cmake. "
                          'For example: -G "Visual Studio 15 2017 Win64"')
+    ap.add_argument("-j", required=False, type=int, default=0,
+                    help="Number of jobs run in parallel")
 
     args = ap.parse_args()
 
@@ -127,13 +121,13 @@ def main():
         return
 
     if args.all or args.usd:
-        usd()
+        usd(args.j)
 
     if args.all or args.hdrpr:
-        hdrpr(args.G)
+        hdrpr(args.G, args.j)
 
     if args.all or args.mx:
-        materialx(args.G)
+        materialx(args.G, args.j)
 
     if args.all or args.libs:
         libs()
