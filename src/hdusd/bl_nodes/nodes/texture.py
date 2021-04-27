907885d089a63b6cc080a4e387b554d9f9f0e915
Mikhail Merzlyutin <merzlyutinme@gmail.com>
2021-04-27 20:02:11 +0300

    Saved packed and edited images to temporary file

--- src/hdusd/bl_nodes/nodes/texture.py
+++ src/hdusd/bl_nodes/nodes/texture.py
@@ -15,6 +15,7 @@
 import os
 
 from ..node_parser import NodeParser
+from ... import utils
 from . import log
 
 
@@ -23,31 +24,32 @@ TEXTURE_ERROR_COLOR = (1.0, 0.0, 1.0)  # following Cycles color for wrong Textur
 
 class ShaderNodeTexImage(NodeParser):
     def export(self):
-        image = self.node.image
-        if not image:
         image_error_result = self.node_item(TEXTURE_ERROR_COLOR)
-            log.warn(f"No image provided")
+        image = self.node.image
+
+        # TODO support UDIM Tilesets and SEQUENCE
+        if not image or image.source in ('TILED', 'SEQUENCE'):
             return image_error_result
-        # TODO support SEQUENCE image type
-        if image.source != 'FILE':
-            log.warn(f"Image {image} is not a file")
+
+        # there were scenes in Linux that have 0x0x0 image packed
+        if image.size[0] * image.size[1] * image.channels == 0:
             return image_error_result
 
         file_path = image.filepath_from_user()
-        # TODO export images from pixels, including is_dirty images
-        if not os.path.isfile(file_path):
-            log.warn(f"Image {image} file doesn't exist at {file_path}")
-            return image_error_result
-        if image.size[0] * image.size[1] * image.channels == 0:
-            log.warn(f"Image {image} has no data")
-            return image_error_result
+        if image.source != 'FILE' or image.is_dirty or not os.path.isfile(file_path):  # generated, edited and packed
+            temp_path = utils.get_temp_file(f"{hash(image.name)}.texture")
+            if not os.path.isfile(temp_path):
+                image.save_render(temp_path)
+            img_path = str(temp_path)
+        else:
+            img_path = file_path
 
         # TODO use Vector input for UV
         uv = self.create_node('texcoord', 'vector2', {})
 
-        result = self.create_node('image', 'color4', {
+        result = self.create_node('image', 'color3', {
             'texcoord': uv,
         })
-        result.set_parameter('file', file_path)
+        result.set_parameter('file', img_path)
 
         return result
