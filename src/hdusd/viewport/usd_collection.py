5ef0bf95a4a60b0ca6ad221d49bc92a0f9168fa0
Bogdan Nagirniak <bnagirniak@luxoft.com>
2021-06-22 21:23:40 +0300

    Improved algorithm of creating objects for USD collection

--- src/hdusd/viewport/usd_collection.py
+++ src/hdusd/viewport/usd_collection.py
@@ -14,6 +14,8 @@
 #********************************************************************
 import bpy
 
+from pxr import Sdf
+
 from ..utils import logging
 log = logging.Log(tag='usd_collection')
 
@@ -22,21 +24,54 @@ COLLECTION_NAME = "USD NodeTree"
 
 
 def update(context):
-    clear(context)
-
     usd_tree_name = context.scene.hdusd.viewport.data_source
     if not usd_tree_name:
+        clear(context)
         return
 
     output_node = bpy.data.node_groups[usd_tree_name].get_output_node()
     if not output_node:
+        clear(context)
         return
 
     stage = output_node.cached_stage()
     if not stage:
+        clear(context)
         return
 
-    create(context, stage)
+    collection = bpy.data.collections.get(COLLECTION_NAME)
+    if not collection:
+        collection = bpy.data.collections.new(COLLECTION_NAME)
+        context.scene.collection.children.link(collection)
+        log("Collection created", collection)
+
+    objects = {}
+    for obj in collection.objects:
+        if obj.hdusd.is_usd:
+            objects[obj.hdusd.sdf_path] = obj
+    obj_paths = set(objects.keys())
+
+    prim_paths = set()
+    for prim in stage.TraverseAll():
+        prim_paths.add(str(prim.GetPath()))
+
+    paths_to_remove = obj_paths - prim_paths
+    paths_to_add = prim_paths - obj_paths
+
+    for path in paths_to_remove:
+        obj = objects.pop(path)
+        bpy.data.objects.remove(obj)
+
+    for path in sorted(paths_to_add):
+        parent_path = str(Sdf.Path(path).GetParentPath())
+        parent_obj = None if parent_path == '/' else objects[parent_path]
+
+        prim = stage.GetPrimAtPath(path)
+        obj = bpy.data.objects.new('/', None)
+        obj.hdusd.sync_from_prim(parent_obj, prim)
+        collection.objects.link(obj)
+
+        objects[path] = obj
 
 
 def clear(context):
@@ -49,24 +84,6 @@ def clear(context):
             bpy.data.objects.remove(obj)
 
     bpy.data.collections.remove(collection)
-
-
-def create(context, stage):
-    collection = bpy.data.collections.get(COLLECTION_NAME)
-    if not collection:
-        collection = bpy.data.collections.new(COLLECTION_NAME)
-        context.scene.collection.children.link(collection)
-        log("Collection created", collection)
-
-    def create_objects(root_obj, root_prim):
-        for prim in root_prim.GetAllChildren():
-            obj = bpy.data.objects.new('/', None)
-            obj.hdusd.sync_from_prim(root_obj, prim)
-            collection.objects.link(obj)
-
-            create_objects(obj, prim)
-
-    create_objects(None, stage.GetPseudoRoot())
 
 
 def scene_save_pre():
