2c4f8749fe64e58ba87d7f47669e21f7f3b45618
Bogdan Nagirniak <bnagirniak@luxoft.com>
2021-08-13 19:03:20 +0300

    Fixed UI

--- src/hdusd/mx_nodes/nodes/base_node.py
+++ src/hdusd/mx_nodes/nodes/base_node.py
@@ -167,8 +167,6 @@ class MxNode(bpy.types.ShaderNode):
 
         nodedef = self.prop.nodedef()
 
-        files = ["file", "filex", "filey", "filez"]
-
         if self._ui_folders:
             col = layout.column(align=True)
             r = None
@@ -189,17 +187,18 @@ class MxNode(bpy.types.ShaderNode):
             if f and not getattr(self, self._folder_prop_name(f)):
                 continue
 
-            if mx_param.getName() in files and self.category in ("texture2d", "texture3d"):
-                title = mx_param.getName() if mx_param.getName() != "file" else ""
-                if title:
-                    layout.template_ID(self.prop, MxNodeDef._param_prop_name(mx_param.getName()),
-                                       open="image.open", new="image.new", text=title)
-                else:
-                    layout.template_ID(self.prop, MxNodeDef._param_prop_name(mx_param.getName()),
-                                       open="image.open", new="image.new")
+            name = mx_param.getName()
+            if self.category in ("texture2d", "texture3d") and mx_param.getType() == 'filename':
+                split = layout.row(align=True).split(factor=0.25, align=True)
+                col = split.column()
+                col.label(text=mx_param.getAttribute('uiname') if mx_param.hasAttribute('uiname')
+                          else title_str(name))
+                col = split.column()
+                col.template_ID(self.prop, MxNodeDef._param_prop_name(name),
+                                open="image.open", new="image.new")
 
             else:
-                layout.prop(self.prop, MxNodeDef._param_prop_name(mx_param.getName()))
+                layout.prop(self.prop, MxNodeDef._param_prop_name(name))
 
     # COMPUTE FUNCTION
     def compute(self, out_key, **kwargs):
