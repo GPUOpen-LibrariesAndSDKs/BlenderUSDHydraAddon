255b29406f8bd6f1c36af4d0910441caae0016bb
Bogdan Nagirniak <bnagirniak@luxoft.com>
2021-08-16 21:31:27 +0300

    RPRBLND-1770: Blender Material Library: Online Integration. Preparations

--- src/hdusd/ui/matlib.py
+++ src/hdusd/ui/matlib.py
@@ -3,30 +3,27 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #********************************************************************
-from .utils import logging
+import bpy
 
-logging.limit_log('', level_show_min=logging.INFO)
+from . import HdUSD_Panel
 
-matlib_url = "https://matlibapi.cistest.luxoft.com/api"
 
-try:
-    # configdev.py example for logging setup:
-    # from .utils import logging
-    # logging.limit_log('default', logging.DEBUG)
-    # from . import config
-    # config.<parameter> = True
+class HDUSD_MATLIB_PT_matlib(HdUSD_Panel):
+    bl_label = ""
+    bl_context = "material"
 
-    from . import configdev
-    logging.info('Loaded configdev', tag='')
+    def draw(self, context):
+        layout = self.layout
 
-except ImportError:
-    pass
+    
+
+
