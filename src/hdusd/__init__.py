ecff4f6f86b74e07e892d752ec59f15a27027159
radeonprorender <radeonprorender.buildmaster@gmail.com>
2021-10-27 19:27:34 +0300

    buildmaster: version update to 1.0.40

--- src/hdusd/__init__.py
+++ src/hdusd/__init__.py
@@ -16,7 +16,7 @@
 bl_info = {
     "name": "USD Hydra",
     "author": "AMD",
-    "version": (1, 0, 39),
+    "version": (1, 0, 40),
     "blender": (2, 93, 0),
     "location": "Info header, render engine menu",
     "description": "USD Hydra rendering plugin for Blender",
