344af626c0784953e7eba9971f5a1d2a25848ae3
markyandex@gmail.com <markyandex@gmail.com>
2022-01-18 18:09:04 +0300

    set env var "HD_ENABLE_SCENE_INDEX_EMULATION" to "false"

--- src/hdusd/__init__.py
+++ src/hdusd/__init__.py
@@ -27,6 +27,7 @@ bl_info = {
 }
 version_build = ""
 
+import os
 
 from . import config
 from .utils import logging
@@ -35,6 +36,7 @@ from .utils import logging
 log = logging.Log('init')
 log.info(f"Loading USD Hydra addon version={bl_info['version']}, build={version_build}")
 
+os.environ["HD_ENABLE_SCENE_INDEX_EMULATION"] = "false"
 
 from . import engine, properties, ui, usd_nodes, mx_nodes, bl_nodes
 
