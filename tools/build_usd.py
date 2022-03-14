c06c3b2b325f5fa9c6a80f2a0fd6d4718a66dd5e
markyandex@gmail.com <markyandex@gmail.com>
2022-03-14 18:28:08 +0300

    Added comment

--- tools/build_usd.py
+++ tools/build_usd.py
@@ -41,7 +41,10 @@ in USD repository.
     os.chdir(str(usd_dir))
 
     try:
-        # applying patch data/USD.path
+        # applying patch data/USD_MaterialX.patch
+        # Temporary implements https://github.com/PixarAnimationStudios/USD/pull/1610
+        # TODO: remove this after up USD to >= 2203 and implement their own fix
+        #  https://github.com/PixarAnimationStudios/USD/commit/adfc04eea92b91965b0da68503539b079a5d30d9
         check_call('git', 'apply', str(repo_dir / "tools/data/USD_MaterialX.patch"))
 
         # modifying pxr/usdImaging/CMakeLists.txt
