b4139046ca52070b75eb14d62ccf97735f904e93
Georgiy <42409851+DagerD@users.noreply.github.com>
2022-03-16 18:41:20 +0300

    BLEN-51: Fix materials on CIS after updating USD. (#216)
    PURPOSE
    
    Fix materials on CIS builds after updating USD.
    
    EFFECT OF CHANGE
    
    Materials works properly on CIS Builds.

--- tools/build_usd.py
+++ tools/build_usd.py
@@ -41,6 +41,12 @@ in USD repository.
     os.chdir(str(usd_dir))
 
     try:
+        # applying patch data/USD_MaterialX.patch
+        # Temporary implements https://github.com/PixarAnimationStudios/USD/pull/1610
+        # TODO: remove this after up USD to >= 2203 and implement their own fix
+        #  https://github.com/PixarAnimationStudios/USD/commit/adfc04eea92b91965b0da68503539b079a5d30d9
+        check_call('git', 'apply', str(repo_dir / "tools/data/USD_MaterialX.patch"))
+
         # modifying pxr/usdImaging/CMakeLists.txt
         usd_imaging_lite_path = repo_dir / "deps/UsdImagingLite/pxr/usdImaging/usdImagingLite"
 
