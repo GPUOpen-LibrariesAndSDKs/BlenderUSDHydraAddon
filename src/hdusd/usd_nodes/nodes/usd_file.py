42332b9db96cb1ad939dc5eab29702a4aa569016
markyandex@gmail.com <markyandex@gmail.com>
2022-05-20 11:59:10 +0300

    fix issue

--- src/hdusd/usd_nodes/nodes/usd_file.py
+++ src/hdusd/usd_nodes/nodes/usd_file.py
@@ -130,6 +130,8 @@ class UsdFileNode(USDNode):
             return None
 
         input_stage = Usd.Stage.Open(file_path)
+        root_layer = input_stage.GetRootLayer()
+        root_layer.TransferContent(input_stage.Flatten(False))
 
         if self.filter_path == '/*':
             set_timesamples_for_stage(input_stage,
