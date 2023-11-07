f10fd3e83dfd1fe8611d17140c3c384cbdf0e2ad
radeonprorender <radeonprorender.buildmaster@gmail.com>
2023-11-07 22:44:42 +0100

    buildmaster: version update to 2.0.7

--- src/hydrarpr/__init__.py
+++ src/hydrarpr/__init__.py
@@ -16,7 +16,7 @@
 bl_info = {
     "name": "Hydra render engine: RPR",
     "author": "AMD",
-    "version": (2, 0, 6),
+    "version": (2, 0, 7),
     "blender": (4, 0, 0),
     "location": "Info header > Render engine menu",
     "description": "Radeon™ ProRender delegate for Hydra render engine",
